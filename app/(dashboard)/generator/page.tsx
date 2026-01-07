fe7a8da933cf1b2572456703850a7f6689b006a8
TodaRyooo <todaryooo@gmail.com>
2026-01-08 05:29:16 +0900

    feat: add animation

--- app/(dashboard)/generator/page.tsx
+++ app/(dashboard)/generator/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import styles from "@/app/(dashboard)/generator/page.module.scss";
 import { useState } from "react";
 import { Text } from "@/components/composites/text";
 import { GeneratorForm } from "./_components/generator-form";
 import { StorageView } from "./_components/storage-view";
 import { ViewToggle } from "./_components/view-toggle";
+import { cn } from "@/lib/utils";
 
 export default function Page() {
   const [view, setView] = useState<"generator" | "storage">("generator");
@@ -13,15 +15,33 @@ export default function Page() {
     <div className="mx-auto flex min-h-[calc(100vh-64px)] w-full flex-col justify-start px-8 py-10 md:px-16">
       <div className="mb-12 space-y-2 text-center">
         <ViewToggle currentView={view} onViewChange={setView} />
-        <Text variant="h1">{view === "generator" ? "Generator" : "Storage"}</Text>
-        <Text variant="muted">
-          {view === "generator"
-            ? "Manage your security with precision."
-            : "Access and organize your saved credentials."}
-        </Text>
+        <div className={styles.textStack}>
+          <Text variant="h1" className={cn(styles.textItem, view === "generator" ? styles.visible : styles.hidden)}>
+            Generator
+          </Text>
+          <Text variant="h1" className={cn(styles.textItem, view === "storage" ? styles.visible : styles.hidden)}>
+            Storage
+          </Text>
+        </div>
+
+        <div className={styles.mutedStack}>
+          <Text variant="muted" className={cn(styles.textItem, view === "generator" ? styles.visible : styles.hidden)}>
+            Manage your security with precision.
+          </Text>
+          <Text variant="muted" className={cn(styles.textItem, view === "storage" ? styles.visible : styles.hidden)}>
+            Access and organize your saved credentials.
+          </Text>
+        </div>
       </div>
 
-      {view === "generator" ? <GeneratorForm /> : <StorageView />}
+      <div className={styles.viewContainer}>
+        <div className={cn(styles.viewContent, view === "generator" ? styles.visible : styles.hidden)}>
+          <GeneratorForm />
+        </div>
+        <div className={cn(styles.viewContent, view === "storage" ? styles.visible : styles.hidden)}>
+          <StorageView />
+        </div>
+      </div>
     </div>
   );
 }
