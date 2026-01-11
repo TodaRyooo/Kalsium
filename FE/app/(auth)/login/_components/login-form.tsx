c474dae505fba803503e66dd1503dbed18545637
TodaRyooo <todaryooo@gmail.com>
2026-01-12 05:04:45 +0900

    feat: Implementation of validation

--- FE/app/(auth)/login/_components/login-form.tsx
+++ FE/app/(auth)/login/_components/login-form.tsx
@@ -10,6 +10,12 @@ import useSWRMutation from "swr/mutation";
 import { postReq } from "@/lib/fetcher";
 import { toast } from "sonner";
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { FormScheme } from "@/lib/valids/login";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { CustomFormField } from "@/components/composites/form";
 
 export const LoginForm = () => {
   const router = useRouter();
@@ -23,13 +29,18 @@ export const LoginForm = () => {
     if (token || (cache && cache !== "null")) router.push("/generator");
   }, [token, router]);
 
-  const handleSignIn = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget as HTMLFormElement);
-    const body = Object.fromEntries(formData);
+  const form = useForm<z.infer<typeof FormScheme>>({
+    resolver: zodResolver(FormScheme),
+    mode: "onChange",
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
+  const handleSignIn = async (v: z.infer<typeof FormScheme>) => {
     try {
-      const res = await trigger(body);
+      const res = await trigger(v);
       setToken(res.token);
       setUser(res.user);
       toast.success("Login successful!");
@@ -40,13 +51,24 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSignIn} className="grid gap-6">
-      <div className="grid gap-4">
-        <Input name="username" id="username" type="email" placeholder="Email Address" />
-        <Input name="password" id="password" type="password" placeholder="Password" />
-      </div>
-      <Separator className="bg-slate-200" />
-      <Button type="submit">{isMutating ? "Signing In..." : "Sign In"}</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSignIn)} className="grid gap-6">
+        <div className="grid gap-4">
+          <CustomFormField
+            control={form.control}
+            fieldName="username"
+            renderChildren={(field) => <Input id="username" type="email" placeholder="Email Address" {...field} />}
+          />
+
+          <CustomFormField
+            control={form.control}
+            fieldName="password"
+            renderChildren={(field) => <Input id="password" type="password" placeholder="Password" {...field} />}
+          />
+        </div>
+        <Separator className="bg-slate-200" />
+        <Button type="submit">{isMutating ? "Signing In..." : "Sign In"}</Button>
+      </form>
+    </Form>
   );
 };
---
+++ FE/lib/valids/login.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const FormScheme = z.object({
+  username: z.email({ message: "You must enter it in your email address format." }),
+  password: z
+    .string()
+    .min(8, {
+      message: "More than 8 characters are required.",
+    })
+    .refine((val) => !/[\s ]/.test(val), {
+      message: "Spaces are not allowed.",
+    })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+      message: "Uppercase, lowercase, and numbers are required.",
+    }),
+});
